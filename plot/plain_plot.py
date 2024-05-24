929bdddf7edd19252ea53f7c9d9a040dc1ee8a1d
Cheng-Yan Song <chysong@cs.nctu.edu.tw>
2024-05-24 23:31:40 +0800

    feat: add plain_plot.py

---
+++ plot/plain_plot.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import mplfinance as mpf
+import argparse
+
+def plot_candlestick(file_path):
+    # 讀取 CSV 文件
+    data = pd.read_csv(file_path)
+
+    # 檢查數據
+    print(data.head())
+
+    # 確保時間列為 datetime 類型
+    data['time'] = pd.to_datetime(data['open_time'])
+    data.set_index('time', inplace=True)
+
+    # 繪製 K 線圖
+    mpf.plot(data, type='candle', style='charles', title='Candlestick Chart', ylabel='Price')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Plot candlestick chart from a CSV file.')
+    parser.add_argument('file_path', type=str, help='The path to the CSV file containing the trading data.')
+
+    args = parser.parse_args()
+    plot_candlestick(args.file_path)
